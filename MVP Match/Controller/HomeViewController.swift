8348ffb88917b7e9493fc70f56530b2838a6e247
m-junaid-butt <junaidbutt.dev@gmail.com>
2022-02-08 15:37:16 +0500

    - Initial Comit - Files Added

---
+++ MVP Match/Controller/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  MVP Match
+//
+//  Created by Junaid Butt on 2/8/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ MVP Match/Utilities/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  MVP Match
+//
+//  Created by Junaid Butt on 2/8/22.
+//
+
+import Foundation
